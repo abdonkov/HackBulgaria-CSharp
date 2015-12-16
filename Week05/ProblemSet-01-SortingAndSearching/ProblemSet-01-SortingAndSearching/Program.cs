85767eab367c14bcafa0b3adc96408de1bd5afc3
abdonkov <baledo@abv.bg>
2015-12-16 02:26:51 +0200

    Added week05 problem set 01

---
+++ Week05/ProblemSet-01-SortingAndSearching/ProblemSet-01-SortingAndSearching/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSet_01_SortingAndSearching
+{
+    class Program
+    {
+        class LastDigitComparer : IComparer<int>
+        {
+            public int Compare(int x, int y)
+            {
+                return x % 10 - y % 10;
+            }
+        }
+
+        class StringLengthComparer : IComparer<string>
+        {
+            public int Compare(string x, string y)
+            {
+                return (x ?? String.Empty).Length - (y ?? String.Empty).Length;
+            }
+        }
+
+        class OddEvenComparer : IComparer<int?>
+        {
+            public int Compare(int? x, int? y)
+            {
+                return (x == null && y == null) ? 0 : (x == null) ? -1 : (y == null) ? 1 : ((x % 2 + y % 2) % 2 == 1) ? (int)(x % 2 - y % 2) : (x % 2 == 0) ? (int)(x - y) : (x % 2 == 1) ? (int)(y - x) : 0;
+
+                //if (x == null && y == null) return 0;
+                //else if (x == null) return -1;
+                //else if (y == null) return 1;
+
+                //if ((x % 2 + y % 2) % 2 == 1) return (int)(x % 2 - y % 2);
+                //else if (x % 2 == 0) return (int)(x - y);
+                //else if (x % 2 == 1) return (int)(y - x);
+
+                //return 0;
+            }
+        }
+
+        class ReverseComparer<T> : IComparer<T>
+        {
+            public int Compare(T x, T y)
+            {
+                return Comparer<T>.Default.Compare(y, x);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[] { 2, 4, 1, 6, 10 };
+            int[] sortedArray1 = array1.BubbleSort().ToArray();
+            Console.WriteLine("Bubble Sort: {0}", string.Join(", ", sortedArray1));
+
+            Console.WriteLine();
+
+            int[] array2 = new int[] { 2, 4, 1, 6, 10 };
+            int[] sortedArray2 = array2.SelectionSort().ToArray();
+            Console.WriteLine("Selection Sort: {0}", string.Join(", ", sortedArray2));
+
+            Console.WriteLine();
+
+            int[] array3 = new int[] { 2, 4, 1, 6, 10 };
+            int[] sortedArray3 = array3.BubbleSort().ToArray();
+            int index = sortedArray3.BSearch(6);
+            Console.WriteLine("Binary Search for 6 in [{0}] returns index: {1}", string.Join(", ", sortedArray3), index);
+
+            Console.WriteLine();
+
+            int[] array4 = new int[] { 2, 4, 1, 6, 10 };
+            int[] sortedArray4 = array4.QuickSort().ToArray();
+            Console.WriteLine("Quick Sort: {0}", string.Join(", ", sortedArray4));
+
+            Console.WriteLine();
+
+            int[] array5 = new int[] { 2, 4, 1, 6, 10 };
+            int[] sortedArray5 = array5.MergeSort().ToArray();
+            Console.WriteLine("Merge Sort: {0}", string.Join(", ", sortedArray5));
+
+            Console.WriteLine();
+
+            int?[] array6 = new int?[] { 2, 4, 1, 6, 11, null, 2, 6, 4, 65, 7, 9, 3, 3, 56, 36, 8, 34, null };
+            int?[] sortedArray6 = array6.QuickSort(new OddEvenComparer()).ToArray();
+            Console.WriteLine("OddEvenComparer: {0}", string.Join(", ", sortedArray6));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week05/ProblemSet-01-SortingAndSearching/ProblemSet-01-SortingAndSearching/SortAndSearchExtensions.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSet_01_SortingAndSearching
+{
+    public static class SortAndSearchExtensions
+    {
+        public static IList<T> BubbleSort<T>(this IList<T> listToSort)
+        {
+            return listToSort.BubbleSort(Comparer<T>.Default);
+        }
+
+        public static IList<T> BubbleSort<T>(this IList<T> listToSort, IComparer<T> comparer)
+        {
+            bool hasChange;
+            do
+            {
+                hasChange = false;
+                for (int i = 0; i < listToSort.Count - 1; i++)
+                {
+                    if (comparer.Compare(listToSort[i], listToSort[i + 1]) > 0)
+                    {
+                        T temp = listToSort[i];
+                        listToSort[i] = listToSort[i + 1];
+                        listToSort[i + 1] = temp;
+                        hasChange = true;
+                    }
+                }
+            }
+            while (hasChange);
+
+            return listToSort;
+        } 
+
+        public static IList<T> SelectionSort<T>(this IList<T> listToSort)
+        {
+            return listToSort.SelectionSort(Comparer<T>.Default);
+        }
+
+        public static IList<T> SelectionSort<T>(this IList<T> listToSort, IComparer<T> comparer)
+        {
+            for (int i = 0; i < listToSort.Count; i++)
+            {
+                int minItemPos = i;
+                for (int j = i + 1; j < listToSort.Count; j++)
+                {
+                    if (comparer.Compare(listToSort[minItemPos], listToSort[j]) > 0)
+                    {
+                        minItemPos = j;
+                    }
+                }
+                T temp = listToSort[i];
+                listToSort[i] = listToSort[minItemPos];
+                listToSort[minItemPos] = temp;
+            }
+
+            return listToSort;
+        }
+
+        public static int BSearch<T>(this IList<T> listForBS, T key)
+        {
+            return listForBS.BSearch(key, Comparer<T>.Default);
+        }
+
+        public static int BSearch<T>(this IList<T> listForBS, T key, IComparer<T> comparer)
+        {
+            int minIndex = 0;
+            int maxIndex = listForBS.Count - 1;
+            while (minIndex <= maxIndex)
+            {
+                int midIndex = minIndex + (maxIndex - minIndex) / 2;
+
+                if (comparer.Compare(listForBS[midIndex], key) == 0) return midIndex;
+                if (comparer.Compare(listForBS[midIndex], key) < 0) minIndex = midIndex + 1;
+                else maxIndex = midIndex - 1;
+
+                if (minIndex < 0 || maxIndex > listForBS.Count - 1) return -1;
+            }
+            return -1;
+        }
+
+        public static IList<T> QuickSort<T>(this IList<T> listToSort)
+        {
+            return listToSort.QuickSort(Comparer<T>.Default);
+        }
+
+        public static IList<T> QuickSort<T>(this IList<T> listToSort, IComparer<T> comparer)
+        {
+            QuickSortRecursion(listToSort, 0, listToSort.Count - 1, comparer);
+            return listToSort;
+        }
+
+        private static void QuickSortRecursion<T>(IList<T> listToSort, int leftIndex, int rightIndex, IComparer<T> comparer)
+        {
+            if (leftIndex < rightIndex)
+            {
+                int partitionPlace = QuickSortPartition(listToSort, leftIndex, rightIndex, comparer);
+
+                QuickSortRecursion(listToSort, leftIndex, partitionPlace - 1, comparer);
+                QuickSortRecursion(listToSort, partitionPlace + 1, rightIndex, comparer);
+            }
+        }
+
+        private static int QuickSortPartition<T>(IList<T> listToSort, int leftIndex, int rightIndex, IComparer<T> comparer)
+        {
+            T pivot = listToSort[rightIndex];
+            int indexToSwap = leftIndex;
+            for (int j = leftIndex; j < rightIndex; j++)
+            {
+                if (comparer.Compare(listToSort[j], pivot) <= 0)
+                {
+                    T temp = listToSort[indexToSwap];
+                    listToSort[indexToSwap] = listToSort[j];
+                    listToSort[j] = temp;
+
+                    indexToSwap++;
+                }
+            }
+            T temp2 = listToSort[indexToSwap];
+            listToSort[indexToSwap] = listToSort[rightIndex];
+            listToSort[rightIndex] = temp2;
+
+            return indexToSwap;
+        }
+
+        public static IList<T> MergeSort<T>(this IList<T> listToSort)
+        {
+            return listToSort.MergeSort(Comparer<T>.Default);
+        }
+
+        public static IList<T> MergeSort<T>(this IList<T> listToSort, IComparer<T> comparer)
+        {
+            var linked = new LinkedList<T>();
+            foreach (T item in listToSort)
+            {
+                linked.AddLast(item);
+            }
+
+            linked = MergeSortLinkedListRecursion(linked, comparer);
+
+            int i = 0;
+            foreach (T item in linked)
+            {
+                listToSort[i++] = item;
+            }
+
+            return listToSort;
+        }
+
+        private static LinkedList<T> MergeSortLinkedListRecursion<T>(LinkedList<T> list, IComparer<T> comparer)
+        {
+            if (list.Count <= 1) return list;
+
+            bool addLeft = true;
+            var left = new LinkedList<T>();
+            var right = new LinkedList<T>();
+            foreach (T item in list)
+            {
+                if (addLeft)
+                {
+                    left.AddLast(item);
+                    addLeft ^= true;
+                }
+                else
+                {
+                    right.AddLast(item);
+                    addLeft ^= true;
+                }
+            }
+
+            left = MergeSortLinkedListRecursion(left, comparer);
+            right = MergeSortLinkedListRecursion(right, comparer);
+
+            return MergeSortLinkedListMerge(left, right, comparer);
+        }
+
+        private static LinkedList<T> MergeSortLinkedListMerge<T>(LinkedList<T> left, LinkedList<T> right, IComparer<T> comparer)
+        {
+            var merged = new LinkedList<T>();
+            while(left.Count !=0 && right.Count != 0)
+            {
+                if (comparer.Compare(left.First.Value, right.First.Value) <= 0)
+                {
+                    merged.AddLast(left.First.Value);
+                    left.RemoveFirst();
+                }
+                else
+                {
+                    merged.AddLast(right.First.Value);
+                    right.RemoveFirst();
+                }
+            }
+
+            while(left.Count != 0)
+            {
+                merged.AddLast(left.First.Value);
+                left.RemoveFirst();
+            }
+            while (right.Count != 0)
+            {
+                merged.AddLast(right.First.Value);
+                right.RemoveFirst();
+            }
+
+            return merged;
+        }
+    }
+}
